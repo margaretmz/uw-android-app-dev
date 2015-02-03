b8aa338a66db7e04a290b97dc8554c5632ea726f
Margaret <margaretmz@gmail.com>
2015-02-03 15:47:12 -0800

    Pushing SampleList - use ListActivity for the simple list.

--- SampleList/app/src/main/java/edu/uw/aad/mzm/sample/samplelist/SimpleListActivity.java
+++ SampleList/app/src/main/java/edu/uw/aad/mzm/sample/samplelist/SimpleListActivity.java
@@ -1,11 +1,16 @@
 package edu.uw.aad.mzm.sample.samplelist;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class SimpleListActivity extends ActionBarActivity {
+/**
+ * A simple list that uses Android default list item layout
+ */
+public class SimpleListActivity extends ListActivity {
 
 //    private String[] androidAPIsShortList = {"ICS", "Jelly Bean", "Kitkat", "Lollipop"};
     private String[] androidAPIs;
@@ -15,12 +20,20 @@ public class SimpleListActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_list);
 
+        // get the string array resources
         androidAPIs = getResources().getStringArray(R.array.androidapis);
 
-        ListView listView = (ListView) findViewById(R.id.listViewSimple);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,                    // Context
-                                            android.R.layout.simple_list_item_1,    // layout for the list item, use a default Android layout
+//        ListView listView = (ListView) findViewById(R.id.listViewSimple);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,               // Context
+                                            android.R.layout.simple_list_item_1,    // default Android layout for the list item
                                             androidAPIs);                           // data to show in the list
-        listView.setAdapter(adapter);
+//        listView.setAdapter(adapter);
+        this.setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(this, "You clicked on " + androidAPIs[position], Toast.LENGTH_SHORT).show();
+
     }
 }
