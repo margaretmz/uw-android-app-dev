c79675781846604b7d8ce5453cc85ee9df37229b
Margaret <margaretmz@gmail.com>
2015-02-08 07:39:36 -0800

    Moving SampleViewPager to /samples.

--- samples/SampleViewPager/app/src/main/java/edu/uw/aad/mzm/sample/viewpager/MainActivity.java
+++ samples/SampleViewPager/app/src/main/java/edu/uw/aad/mzm/sample/viewpager/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by Margaret on 2/4/2015
- * This is a sample app demo how to create a ViewPager in Android
+ * This is a sample app demos how to create a ViewPager in Android
+ * 1. Create SectionFragment
+ * 2. Create SectionsPagerAdapter extends from FragmentPagerAdapter
+ * 3. In activity_main.xml add ViewPager and PagerTabStrip
+ * 4. In MainActivity.java set up the ViewPager and the FragmentPagerAdapter
  */
 public class MainActivity extends ActionBarActivity {
 
