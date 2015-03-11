d30d5de205c4d362e650a1a21e32e01d18af6ac5
Margaret <margaretmz@gmail.com>
2015-03-11 12:52:35 -0700

    Pushing SampleAsyncTask - remove menu from main screen.

--- samples/SampleAsyncTask/app/src/main/java/edu/uw/aad/mzm/sample/asynctask/MainActivity.java
+++ samples/SampleAsyncTask/app/src/main/java/edu/uw/aad/mzm/sample/asynctask/MainActivity.java
@@ -1,9 +1,7 @@
 package edu.uw.aad.mzm.sample.asynctask;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -31,28 +29,5 @@ public class MainActivity extends ActionBarActivity {
                 imageDownloadTask.execute(url);
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
